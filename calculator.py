af9efc7c42544c8fc892b2ef4b5412602222199d
Bharat Thakur <equinoxter707@gmail.com>
2025-07-10 12:34:49 +0530

    Added multiply function

--- calculator.py
+++ calculator.py
@@ -2,9 +2,12 @@ def add(a,b):
     return a + b
 def subtract(a,b):
     return a - b
+def multi(a,b):
+    return a*b
 
 a = int(input("Enter the 1st number"))
 b = int(input("Enter the 2nd number"))
 
 print(add(a,b))
 print(subtract(a,b))
+print(multi(a,b))
