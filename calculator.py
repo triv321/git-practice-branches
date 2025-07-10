cd672ecb5d054c04fc6ff1c77b194390f76e0bc9
Bharat Thakur <equinoxter707@gmail.com>
2025-07-10 12:30:32 +0530

    Initial commit with calculator functions

---
+++ calculator.py
@@ -0,0 +1,10 @@
+def add(a,b):
+    return a + b
+def subtract(a,b):
+    return a - b
+
+a = int(input("Enter the 1st number"))
+b = int(input("Enter the 2nd number"))
+
+print(add(a,b))
+print(subtract(a,b))
